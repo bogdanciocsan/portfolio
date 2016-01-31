19807122ad7b486c0b1ec08ae32f0b48fd059525
bogdanciocsan <bogdanciocsan@gmail.com>
2016-01-31 18:58:47 +0100

    added php routing (.htaccess)

--- js/routes/routes.js
+++ js/routes/routes.js
@@ -1,16 +1,19 @@
 
-portfolioApp.config(['$routeProvider',
-              function($routeProvider) {
-                  $routeProvider.
-                  when('/web', {
-                      templateUrl: 'views/web.html',
-                      controller: 'WebCtrl'
-                  }).
-                  when('/photos', {
-                      templateUrl: 'views/photos.html',
-                      controller: 'PhotosCtrl'
-                  }).
-                  otherwise({
-                      redirectTo: '/web'
-                  });
-              }]);
+portfolioApp.config(['$routeProvider','$locationProvider',
+                     function($routeProvider, $locationProvider) {
+                         $routeProvider.
+                         when('/web', {
+                             templateUrl: 'views/web.html',
+                             controller: 'WebCtrl'
+                         }).
+                         when('/photos', {
+                             templateUrl: 'views/photos.html',
+                             controller: 'PhotosCtrl'
+                         }).
+                         otherwise({
+                             redirectTo: '/web'
+                         });
+                         $locationProvider.html5Mode(true);
+
+                         $locationProvider.hashPrefix('!');
+                     }]);

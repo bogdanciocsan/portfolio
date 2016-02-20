7281a7017d1baebaf492282b21cd7679fa78e50a
Bogdan Ciocsan <design@bogdanciocsan.com>
2016-02-20 12:28:49 +0100

    Updated css / js minification

---
+++ plugins/angularjs.js
@@ -0,0 +1,8 @@
+
+// @koala-prepend  '../bower_components/angular/angular.js'
+// @koala-append  '../bower_components/angular-masonry/angular-masonry.js'
+// @koala-append  '../bower_components/angular-resource/angular-resource.js'
+// @koala-append  '../bower_components/angular-route/angular-route.js'
+// @koala-append  '../bower_components/ng-dialog/js/ngDialog.js'
+// @koala-append  'prettify/prettify.js'
+// @koala-append  'prettify/lang-css.js'
